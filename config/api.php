ba46b81a550c9a57b34ca1655961db5c2f716fc2
Jérémie Mondino <jmondino.work@gmail.com>
2026-06-15 00:15:34 +0200

    DA commit

---
+++ config/api.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+    'SENDINBLUE_API_KEY' => '########',
+
+    'SIRENE_CONSUMER_KEY' => '########',
+    'SIRENE_CONSUMER_SECRET' => '########',
+
+    'ION_ACCOUNT_ID' => '1',
+
+    'OPENAI_API_KEY' => '########',
+
+    'MS_CLIENT_ID' => '########',
+    'MS_CLIENT_SECRET' => '########',
+
+    'GOOGLE_CLIENT_ID' => '########.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET' => '########',
+
+    'PAYFIT_API_KEY' => '########',
+    'PAYFIT_API_SECRET' => '########',
+    'PAYFIT_URL_AUTHORIZE' => 'https://oauth.payfit.com/authorize',
+    'PAYFIT_URL_TOKEN' => 'https://oauth.payfit.com/token',
+    'PAYFIT_URL_RESSOURCE' => '',
+    'PAYFIT_SCOPE' => 'collaborators:read collaborators:management:read collaborators:personal:read contracts:read collaborators:contracts:read',
+    'PAYFIT_URL_COMPANY' => 'https://oauth.payfit.com/introspect',
+    'PAYFIT_URL_EMPLOYEES' => 'https://partner-api.payfit.com/companies/{companyId}/collaborators',
+
+    'LUCCA_URL_EMPLOYEES' => '{companyDomain}/api/v3/users'
+
+];
